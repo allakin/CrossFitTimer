f8500f46f2d3cacc76b8ca538166d243d6861b79
Pavel <19paul91@mail.ru>
2017-01-01 13:23:07 +0300

    work label display

--- CrossFitTimer/ViewController.swift
+++ CrossFitTimer/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 	@IBOutlet weak var startStopButton: UIBarButtonItem!
 	@IBOutlet weak var lapResetButton: UIBarButtonItem!
 
+	var timer = Timer()
+	var minutes: Int = 0
+	var seconds: Int = 0
+	var fractions: Int = 0
+	var startStopWatch: Bool = true
+	var addLap: Bool = false
+	var stopWatchString: String = ""
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -27,10 +34,44 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 	}
 
 	@IBAction func startStopButton(_ sender: Any) {
+		
+		if startStopWatch == true {
+			timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateStopWatch), userInfo: nil, repeats: true)
+			startStopWatch = false
+			startStopButton.image = UIImage(named: "close.png")
+			addLap = true
+		} else {
+			startStopWatch = true
+			timer.invalidate()
+			startStopButton.image = UIImage(named: "play.png")
+		}
+		
 	}
 	@IBAction func lapRefreshButton(_ sender: Any) {
 	}
 
+	func updateStopWatch() {
+		fractions += 1
+		if fractions == 100 {
+			seconds += 1
+			fractions = 0
+		} else if seconds == 60 {
+			minutes += 1
+			seconds = 0
+		} else if seconds == 60 {
+			minutes += 1
+			seconds = 0
+		}
+	
+		let fractionsString = fractions > 9 ? "\(fractions)" : "0\(fractions)"
+		let secondString = seconds > 9 ? "\(seconds)" : "0\(seconds)"
+		let minutesString = minutes > 9 ? "\(minutes)" : "0\(minutes)"
+		
+		stopWatchString = "\(minutesString):\(secondString):\(fractionsString)"
+		stopWatchLabel.text = stopWatchString
+		
+	}
+	
 	//MARK: - Table View Methods
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 	return 3
